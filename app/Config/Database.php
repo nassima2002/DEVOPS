082e199790300f04f0e74e4d53121c5585a5549f
douniakritet <kritetdounia@gmail.com>
2025-04-21 12:10:16 +0100

    User push

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,13 +27,13 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
+        'username'     => 'root',
         'password'     => '',
-        'database'     => '',
+        'database'     => 'ci4_tests',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
-        'DBDebug'      => true,
+        'DBDebug'      => (ENVIRONMENT !== 'production'),
         'charset'      => 'utf8mb4',
         'DBCollat'     => 'utf8mb4_general_ci',
         'swapPre'      => '',
@@ -160,35 +160,35 @@ class Database extends Config
     /**
      * This database connection is used when running PHPUnit database tests.
      *
-     * @var array<string, mixed>
-     */
-    public array $tests = [
-        'DSN'         => '',
-        'hostname'    => '127.0.0.1',
-        'username'    => '',
-        'password'    => '',
-        'database'    => ':memory:',
-        'DBDriver'    => 'SQLite3',
-        'DBPrefix'    => 'db_',  // Needed to ensure we're working correctly with prefixes live. DO NOT REMOVE FOR CI DEVS
-        'pConnect'    => false,
-        'DBDebug'     => true,
-        'charset'     => 'utf8',
-        'DBCollat'    => '',
-        'swapPre'     => '',
-        'encrypt'     => false,
-        'compress'    => false,
-        'strictOn'    => false,
-        'failover'    => [],
-        'port'        => 3306,
-        'foreignKeys' => true,
-        'busyTimeout' => 1000,
-        'dateFormat'  => [
-            'date'     => 'Y-m-d',
-            'datetime' => 'Y-m-d H:i:s',
-            'time'     => 'H:i:s',
-        ],
-    ];
-
+    /**  * @var array<string, mixed>*/
+    /** */
+    //public array $tests = [
+      //  'DSN'         => '',
+       // 'hostname'    => '127.0.0.1',
+        //'username'    => '',
+        //'password'    => '',
+        //'database'    => ':memory:',
+        //'DBDriver'    => 'SQLite3',
+        //'DBPrefix'    => 'db_',  // Needed to ensure we're working correctly with prefixes live. DO NOT REMOVE FOR CI DEVS
+        //'pConnect'    => false,
+        //'DBDebug'     => true,
+        //'charset'     => 'utf8',
+        //'DBCollat'    => '',
+        //'swapPre'     => '',
+        //'encrypt'     => false,
+        //'compress'    => false,
+        //'strictOn'    => false,
+        //'failover'    => [],
+        //'port'        => 3306,
+        //'foreignKeys' => true,
+        //'busyTimeout' => 1000,
+        //'dateFormat'  => [
+          //  'date'     => 'Y-m-d',
+            //'datetime' => 'Y-m-d H:i:s',
+            //'time'     => 'H:i:s',
+        //],
+    //];
+ 
     public function __construct()
     {
         parent::__construct();
